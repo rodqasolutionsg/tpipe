a6e15d1e46f65000355a0c8a37d556669b8a9926
student <rod.qasolutions@gmail.com>
2021-03-27 17:20:25 +0000

    Comit CISummers

---
+++ src/main/java/com/caltechs/CISummers/CiSummersApplication.java
@@ -0,0 +1,13 @@
+package com.caltechs.CISummers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiSummersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiSummersApplication.class, args);
+	}
+
+}
